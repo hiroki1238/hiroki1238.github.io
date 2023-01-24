0cf40523758053eceae2c40749b7706c3f63dba8
Hiroki1238 <c14h14n3nao3s@gmail.com>
2023-01-24 18:47:54 +0900

    [add]詳細ページ(about)の追加

---
+++ src/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <div className="mt-16 ml-16">
+      <p className="text-4xl mb-16">私たちについて</p>
+    </div>
+  );
+}
--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-red-500 text-3xl">neko</p>
+    <div className="mt-16 ml-16">
+      <p className="text-4xl mb-16">株式会社たば</p>
+      <div className="flex">
+        <div>
+          <Link href="/about">私たちについて</Link>
+          <br />
+          <Link href="/about">私たちについて</Link>
+          <br />
+          <Link href="/about">私たちについて</Link>
+        </div>
+        <div>
+          <img src="./image/office.jpg" className="w-4/5 ml-auto mr-auto" />
+        </div>
+      </div>
     </div>
   );
 }
