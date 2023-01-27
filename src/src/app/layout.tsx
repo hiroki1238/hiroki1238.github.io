71d8894ff0fd713d3c0b2239b4159d133ea89bea
Hiroki1238 <c14h14n3nao3s@gmail.com>
2023-01-27 21:44:54 +0900

    [fix]ヘッダーのデザインの変更

--- src/src/app/layout.tsx
+++ src/src/app/layout.tsx
@@ -17,51 +17,62 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <section className="font-kosugimaru"> {/* フォント指定 */}
+      <section className="font-kosugimaru">
+        {" "}
+        {/* フォント指定 */}
         <header className="bg-white h-10 pb-32">
-          <nav className="pt-12">
-          <Link
+          <nav className="pt-12 flex">
+            <Link
               href="/description"
-              className="mr-6 no-underline text-3xl text-gray-500 hover:text-gray600"
+              className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
             >
               株式会社たば（ロゴを設置）
             </Link>
 
-            <Link
-              href="/description"
-              className="mr-6 no-underline text-3xl text-gray-500 hover:text-gray600"
-            >
-              会社概要
-            </Link>
-            <Link
-              href="/description"
-              className="mr-6 no-underline text-3xl text-gray-500 hover:text-gray600"
-            >
-              サービス
-            </Link>
-            <Link
-              href="/description"
-              className="mr-6 no-underline text-3xl text-gray-500 hover:text-gray600"
-            >
-              ニュース
-            </Link>
-            <Link
-              href="/description"
-              className="mr-6 no-underline text-3xl text-gray-500 hover:text-gray600"
-            >
-              お問い合わせ
-            </Link>
-            <Link
-              href="/description"
-              className="mr-4 no-underline text-3xl text-gray-500 hover:text-gray600"
-            >
-              採用情報
-            </Link>
+            <div className="ml-auto">
+              <Link
+                href="/description"
+                className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
+              >
+                会社概要
+              </Link>
+
+              <Link
+                href="/description"
+                className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
+              >
+                サービス
+              </Link>
+
+              <Link
+                href="/description"
+                className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
+              >
+                ニュース
+              </Link>
+
+              <Link
+                href="/description"
+                className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
+              >
+                お問い合わせ
+              </Link>
+
+              <Link
+                href="/description"
+                className="mr-16 no-underline text-2xl text-gray-500 hover:text-gray600"
+              >
+                採用情報
+              </Link>
+            </div>
           </nav>
         </header>
         <body>{children}</body>
       </section>
-      <footer className="bg-gray-200 font-kosugimaru"> {/* フォント指定 */}
+
+      <footer className="bg-gray-200 font-kosugimaru">
+        {" "}
+        {/* フォント指定 */}
         <div className="flex">
           <div>
             <p className="pt-11 pr-7">
@@ -107,7 +118,6 @@ export default function RootLayout({
             </p>
           </div>
         </div>
-
         <p className="text-center text-2xl mt-5 py-12 text-gray-600 font-zenmaru font-black">
           Copyright © 2023 TABA CORPORATION. All Rights Reserved.
         </p>
--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -3,8 +3,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
+    <div>
       <div>
         <img src="./image/office.jpg" className="w-full" />
       </div>
+
+      <div className="mt-20 ml-36">
+        <p className="text-6xl text-gray-600 font-black font-zenmaru">ABOUT</p>
+        <p className="text-xl mt-2 text-gray-400">私たちについて</p>
+        <p className="text-2xl mt-10 text-gray-600">「株式会社たば」は2023年2月にHiroki1238によって設立されたポートフォリオサイトです。</p>
+      </div>
+    </div>
   );
 }
