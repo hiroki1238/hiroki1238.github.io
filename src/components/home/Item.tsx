cdb39a9713f98d5d09eea71cca2f1e5b31f9cd43
Hiroki1238 <c14h14n3nao3s@gmail.com>
2023-01-28 23:38:50 +0900

    [fix]layout.jsxのエラーの解消

--- src/components/home/Item.tsx
+++ src/components/home/Item.tsx
@@ -1,37 +1,40 @@
+"use client";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-export default function Item() {
+export default function Item(props: any) {
+  const { title, message, button } = props;
+  const router = useRouter();
+
   return (
-        <div className="mt-20 flex">
-          <div className="w-3/4">
-            <p className="text-6xl text-gray-600 font-black font-zenmaru">
-              {/* ABOUT */}
-            </p>
-            <p className="text-xl mt-2 text-gray-400">私たちについて</p>
-            <p className="text-2xl mt-10 mr-10 text-gray-600">
-              {/* 「株式会社たば」は2023年2月にHiroki1238によって設立されたポートフォリオサイトです。 */}
-            </p>
+    <div className="mt-20 flex">
+      <div className="w-3/4">
+        <p className="text-6xl text-gray-600 font-black font-zenmaru">{title}</p>
+        <p className="text-xl mt-2 text-gray-400">私たちについて</p>
+        <p className="text-2xl mt-10 mr-10 text-gray-600">{message}</p>
+        <button
+          className="mt-10 ml-10 py-4 px-6 text-xl bg-transparent border-2 border-slate-700 hover:border-transparent rounded-xl hover:bg-slate-700 text-slate-700 hover:text-white"
+          type="button" onClick={() => router.push("/about")}>{button}</button>
+        {/* 方法1 */}
 
-            <button className="mt-10 ml-10 py-4 px-6 text-xl bg-transparent border-2 border-slate-700 hover:border-transparent rounded-xl hover:bg-slate-700 text-slate-700 hover:text-white" onClick="loction.href='/about'">
-              {/* 会社概要 */}
-            </button>  {/* 方法1 */}
+        <button className="mt-10 ml-10 py-4 px-6 text-xl bg-transparent border-2 border-slate-700 hover:border-transparent rounded-xl hover:bg-slate-700 text-slate-700 hover:text-white">
+          <Link href="/about">会社概要</Link></button>
+        {/* 方法2 */}
 
-            <button className="mt-10 ml-10 py-4 px-6 text-xl bg-transparent border-2 border-slate-700 hover:border-transparent rounded-xl hover:bg-slate-700 text-slate-700 hover:text-white">
-              <Link href="/about">会社概要</Link>
-            </button>  {/* 方法2 */}
-
-            <Link
-              href="/about"
-              className="mt-10 ml-10 py-5 px-7 text-xl bg-transparent border-2 border-slate-700 hover:border-transparent rounded-xl hover:bg-slate-700 text-slate-700 hover:text-white">
-              会社概要
-            </Link>  {/* 方法3 */}
-          </div>
-          <div className="w-3/7">
-            <img
-              src="./image/city.jpg"
-              className="border border-gray-400 rounded-xl"
-            />
-          </div>
-        </div>
+        <Link
+          href="/about"
+          className="mt-10 ml-10 text-xl bg-transparent border-2 border-slate-700 hover:border-transparent rounded-xl hover:bg-slate-700 text-slate-700 hover:text-white"
+        >
+          会社概要
+        </Link>
+        {/* 方法3 */}
+      </div>
+      <div className="w-3/7">
+        <img
+          src="./image/city.jpg"
+          className="border border-gray-400 rounded-xl"
+        />
+      </div>
+    </div>
   );
 }
--- src/src/app/layout.tsx
+++ src/src/app/layout.tsx
@@ -8,120 +8,120 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Head>
+    <html>
+      <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>株式会社たば</title>
-      </Head>
+      </head>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <section className="font-kosugimaru">
-        {" "}
-        {/* フォント指定 */}
-        <header className="bg-white h-10 pb-32">
-          <nav className="pt-12 flex">
-            <Link
-              href="/description"
-              className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
-            >
-              株式会社たば（ロゴを設置）
-            </Link>
-
-            <div className="ml-auto">
+      <body>
+        <section className="font-kosugimaru">
+          {/* フォント指定 */}
+          <header className="bg-white h-10 pb-32">
+            <nav className="pt-12 flex">
               <Link
-                href="/description"
+                href="/"
                 className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
               >
-                会社概要
+                株式会社たば（ロゴを設置）
               </Link>
 
-              <Link
-                href="/description"
-                className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
-              >
-                サービス
-              </Link>
+              <div className="ml-auto">
+                <Link
+                  href="/"
+                  className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
+                >
+                  会社概要
+                </Link>
 
-              <Link
-                href="/description"
-                className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
-              >
-                ニュース
-              </Link>
+                <Link
+                  href="/"
+                  className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
+                >
+                  サービス
+                </Link>
 
-              <Link
-                href="/description"
-                className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
-              >
-                お問い合わせ
-              </Link>
+                <Link
+                  href="/"
+                  className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
+                >
+                  ニュース
+                </Link>
 
-              <Link
-                href="/description"
-                className="mr-16 no-underline text-2xl text-gray-500 hover:text-gray600"
-              >
-                採用情報
-              </Link>
+                <Link
+                  href="/"
+                  className="mr-6 no-underline text-2xl text-gray-500 hover:text-gray600"
+                >
+                  お問い合わせ
+                </Link>
+
+                <Link
+                  href="/"
+                  className="mr-16 no-underline text-2xl text-gray-500 hover:text-gray600"
+                >
+                  採用情報
+                </Link>
+              </div>
+            </nav>
+          </header>
+          {children}
+        </section>
+
+        <footer className="bg-gray-200 font-kosugimaru">
+          {/* フォント指定 */}
+          <div className="flex">
+            <div>
+              <p className="pt-11 pr-7">
+                <Link
+                  href="/description"
+                  className="no-underline text-3xl text-gray-500 hover:text-gray600"
+                >
+                  仮タイトル
+                </Link>
+              </p>
             </div>
-          </nav>
-        </header>
-        <body>{children}</body>
-      </section>
 
-      <footer className="bg-gray-200 font-kosugimaru">
-        {" "}
-        {/* フォント指定 */}
-        <div className="flex">
-          <div>
-            <p className="pt-11 pr-7">
-              <Link
-                href="/description"
-                className="no-underline text-3xl text-gray-500 hover:text-gray600"
-              >
-                仮タイトル
-              </Link>
-            </p>
-          </div>
+            <div>
+              <p className="pt-11 pr-7">
+                <Link
+                  href="/description"
+                  className="no-underline text-3xl text-gray-500 hover:text-gray600"
+                >
+                  仮タイトル
+                </Link>
+              </p>
+            </div>
 
-          <div>
-            <p className="pt-11 pr-7">
-              <Link
-                href="/description"
-                className="no-underline text-3xl text-gray-500 hover:text-gray600"
-              >
-                仮タイトル
-              </Link>
-            </p>
-          </div>
+            <div>
+              <p className="pt-11 pr-7">
+                <Link
+                  href="/description"
+                  className="no-underline text-3xl text-gray-500 hover:text-gray600"
+                >
+                  仮タイトル
+                </Link>
+              </p>
+            </div>
 
-          <div>
-            <p className="pt-11 pr-7">
-              <Link
-                href="/description"
-                className="no-underline text-3xl text-gray-500 hover:text-gray600"
-              >
-                仮タイトル
-              </Link>
-            </p>
+            <div>
+              <p className="pt-11 pr-24">
+                <Link
+                  href="/description"
+                  className="no-underline text-3xl text-gray-500 hover:text-gray-600"
+                >
+                  仮タイトル
+                </Link>
+              </p>
+            </div>
           </div>
-
-          <div>
-            <p className="pt-11 pr-24">
-              <Link
-                href="/description"
-                className="no-underline text-3xl text-gray-500 hover:text-gray-600"
-              >
-                仮タイトル
-              </Link>
-            </p>
-          </div>
-        </div>
-        <p className="text-center text-2xl mt-5 py-12 text-gray-600 font-zenmaru font-black">
-          Copyright © 2023 TABA CORPORATION. All Rights Reserved.
-        </p>
-      </footer>
-    </>
+          <p className="text-center text-2xl mt-5 py-12 text-gray-600 font-zenmaru font-black">
+            Copyright © 2023 TABA CORPORATION. All Rights Reserved.
+          </p>
+        </footer>
+      </body>
+    </html>
   );
 }
