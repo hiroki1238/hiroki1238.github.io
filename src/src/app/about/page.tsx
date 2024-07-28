dcc9ee7553eb454772310ab569a9ec50cc64c8bd
hiroki1238 <hiroki.taba@wed.company>
2024-07-28 11:44:50 +0900

    Add tool icon display to page

--- src/src/app/about/page.tsx
+++ src/src/app/about/page.tsx
@@ -6,7 +6,9 @@ export default function About() {
       <p className="text-6xl text-gray-600 font-black font-zenmaru">ABOUT US</p>
       <p className="text-xl mt-2 text-gray-400">私たちについて</p>
       <p className="text-2xl mt-10 mr-10 text-gray-600">
-        「株式会社たば」は2023年9月にHiroki Tabaによって設立されたポートフォリオサイトです。株式会社たばは「毎日楽しい」を目標に、こだわりを持って開発したサービスを提供しています。
+        「株式会社たば」は2023年9月にHiroki1238によって開設されたポートフォリオサイトです。
+        <br />
+        世の中を今より少しだけ便利にすることを目標に、様々な分野のサービスを開発・提供しています。
       </p>
       <div className="mb-36"></div>
 
@@ -21,13 +23,17 @@ export default function About() {
         <div className="border-b-2 my-3">2023年9月1日</div>
 
         <div className="border-b-2 my-3 text-center">代表取締社長</div>
-        <div className="border-b-2 my-3">たば ひろき</div>
-
-        <div className="border-b-2 my-3 text-center">資本金</div>
-        <div className="border-b-2 my-3">非公開</div>
+        <div className="border-b-2 my-3 font-zenmaru font-blac">
+          Hiroki Taba
+        </div>
 
         <div className="border-b-2 my-3 text-center">所在地</div>
-        <div className="border-b-2 my-3">〒286-0023 千葉県成田市成田1番地</div>
+        <div className="border-b-2 my-3">千葉県 成田市</div>
+
+        <div className="border-b-2 my-3 text-center">お問い合わせ</div>
+        <div className="border-b-2 my-3 font-zenmaru font-black">
+          hirokitaba@test.com
+        </div>
 
         {/* <div className="border-b-2 my-3 text-center">事業内容</div>
         <div className="border-b-2 my-3">EC事業、DX事業など</div> */}
@@ -41,7 +47,7 @@ export default function About() {
         </p>
         <p className="text-xl mt-2 text-gray-400">沿革</p>
         <div className="text-2xl mt-10 text-gray-600 grid grid-cols-3 w-full">
-          <div className="border-b-2 my-3 text-center">2022年</div>
+          <div className="border-b-2 my-3 text-center width-1/2">2022年</div>
           <div className="border-b-2 my-3 text-center">9月</div>
           <div className="border-b-2 my-3">
             レバテックカレッジ 入社（インターン）
@@ -62,10 +68,6 @@ export default function About() {
           <div className="border-b-2 my-3 text-center">2024年</div>
           <div className="border-b-2 my-3 text-center">?月</div>
           <div className="border-b-2 my-3">???</div>
-
-          <div className="border-b-2 my-3 text-center">2024年</div>
-          <div className="border-b-2 my-3 text-center">?月</div>
-          <div className="border-b-2 my-3">???</div>
         </div>
       </div>
 
@@ -74,11 +76,167 @@ export default function About() {
           EXPERIENCES
         </p>
         <p className="text-xl mt-2 text-gray-400">これまでの経験</p>
-        <p className="mt-6 text-4xl text-gray-600 font-black font-zenmaru">○TOOLS</p>
         <p className="text-2xl mt-10 mr-10 text-gray-600">
           使用経験のある言語、ツールです。
         </p>
 
+        <p className="mt-6 mb-2 text-xl text-gray-600 font-black font-zenmaru">
+          ▪︎業務で使用しているもの
+        </p>
+        <div className="align-bottom flex items-center">
+          <Image
+            className="mr-2"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/swift/swift-original.svg"
+            alt="php"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-2"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+            alt="javascript"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-1"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+            alt="typescript"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-1"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+            alt="css3"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-1"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+            alt="html5"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-1"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+            alt="react"
+            width={60}
+            height={60}
+          />
+          <Image
+            src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+            alt="git"
+            width={60}
+            height={60}
+          />
+        </div>
+        <p className="mt-6 mb-2 text-xl text-gray-600 font-black font-zenmaru">
+          ▪︎その他 使用経験のあるもの
+        </p>
+        <div className="align-bottom flex items-center">
+          <Image
+            className="mr-1"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
+            alt="c"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-2"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"
+            alt="cplusplus"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-2"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"
+            alt="php"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-2"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-original-wordmark.svg"
+            alt="laravel"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-2"
+            src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg"
+            alt="nextjs"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-2"
+            src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+            alt="tailwind"
+            width={60}
+            height={60}
+          />
+
+          <Image
+            className="mr-2"
+            src="https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg"
+            alt="mariadb"
+            width={60}
+            height={60}
+          />
+
+          <Image
+            className="mr-2"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+            alt="mysql"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-2"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+            alt="postgresql"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-2"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg"
+            alt="docker"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-2"
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
+            alt="aws"
+            width={60}
+            height={60}
+          />
+          <Image
+            className="mr-1"
+            src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg"
+            alt="heroku"
+            width={50}
+            height={50}
+          />
+          <Image
+            className="mr-2"
+            src="https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg"
+            alt="unity"
+            width={50}
+            height={50}
+          />
+          <Image
+            src="https://download.blender.org/branding/community/blender_community_badge_white.svg"
+            alt="blender"
+            width={60}
+            height={60}
+          />
+        </div>
       </div>
     </div>
   );
--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -1,25 +1,23 @@
 import Image from "next/image";
-import Link from "next/link";
 import Item from "@/components/home/Item";
 
 export default function Home() {
   return (
     <div>
       <div>
-        <img src="./image/office.jpg" className="w-full h-auto" />
-        {/* <Image
+        <Image
           src="/image/office.jpg"
           width={1000}
           height={1000}
-          alt="Picture of an office"
+          alt="office"
           style={{ width: '100%', height: 'auto' }} // なぜかImageタグだと画質が悪くなる（tailwindの書き方にはなっていない）
-        /> */}
+        />
       </div>
 
       <div className="w-5/7 mr-auto ml-auto">
         <Item
           title="ABOUT"
-          message="「株式会社たば」は2023年9月にHiroki Tabaによって設立されたポートフォリオサイトです。"
+          message="「株式会社たば」は2023年9月にHiroki1238によって開設されたポートフォリオサイトです。"
           button="会社概要"
           path="/about"
           img="./image/city.jpg"
@@ -27,7 +25,7 @@ export default function Home() {
 
         <Item
           title=" SERVICE"
-          message="株式会社たばはより便利な世の中を実現することを目標に、様々な分野のサービスを開発・提供しています。"
+          message="「株式会社たば」は世の中を今より少しだけ便利にすることを目標に、様々な分野のサービスを開発・提供しています。"
           button="サービス一覧"
           path="/about"
           img="./image/service.jpg"
@@ -42,7 +40,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-36">.....</div>
+      <div className="mb-36" />
     </div>
   );
 }
